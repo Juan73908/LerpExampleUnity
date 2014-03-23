51dd06a6dd58f01d590839dfed0b39892dde820a
Juan73908 <juangmgs@hotmail.com>
2014-03-23 19:42:20 +0100

    First commit

---
+++ Assets/Scripts/LerpController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Controlls the interaction with the GUI
+public class LerpController : MonoBehaviour {
+
+	// Assigned in the inspector
+	public LerpScript[] scripts;
+
+	// Lerp variables for the button
+	public float speed = 3.0f;
+
+	public Color color1;
+	public Color color2;
+	protected Color destinationColor;
+
+	public Vector3 rotation1;
+	public Vector3 rotation2;
+	protected Quaternion destinationRotation;
+
+	// Cache variables
+	protected SpriteRenderer spriteRenderer;
+	
+	// Initialization
+	void Start() {
+		destinationColor = color1;
+		destinationRotation.eulerAngles = rotation1;
+
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// When clicked
+	void OnMouseDown(){
+		// Change destination of the Lerp Scripts
+		foreach (LerpScript script in scripts){
+			script.ChangeDestination();
+		}
+
+		// Change color and rotation
+		if (destinationColor == color1){
+			destinationColor = color2;
+			destinationRotation.eulerAngles = rotation2;
+		}
+		else{
+			destinationColor = color1;
+			destinationRotation.eulerAngles = rotation1;
+		}
+	}
+
+	// Lerp every frame
+	void Update (){
+		// Lerp color
+		spriteRenderer.color = Color.Lerp(spriteRenderer.color, destinationColor, speed * Time.deltaTime);
+
+		// Lerp rotation
+		transform.rotation = Quaternion.Lerp(transform.rotation, destinationRotation , speed * 3.0f * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/LerpScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Example script that shows the two most common uses of Lerp.
+public class LerpScript : MonoBehaviour {
+
+	// Assigned in the inspector
+	public Transform position1;
+	public Transform position2;
+
+	// Auxiliar variables
+	protected Transform destination;
+
+	// How fast the character moves
+	public float speed = 1.0f;
+
+	// Two types of lerp
+	public enum LerpType{Constant, Smooth};
+	public LerpType lerpType;
+
+	// Constant Lerp variables
+	protected float timer = 0.0f;
+
+	// Initialization
+	void Start(){
+		destination = position1;
+	}
+
+
+	// Called when the character should move
+	public void ChangeDestination(){
+		// Change the destination
+		if (destination == position1){
+			destination = position2;
+		}
+		else{
+			destination = position1;
+		}
+	}
+
+	// Lerp is done over time
+	void Update(){
+		// Check for the selected type of lerp
+		switch (lerpType) {
+		case LerpType.Constant:
+			// From position1 to position2 with incresing t
+			transform.position = Vector3.Lerp(position1.position, position2.position, speed * timer);
+			break;
+		case LerpType.Smooth:
+			// From actual position to destination with "constant" t
+			transform.position = Vector3.Lerp(transform.position, destination.position, speed  * 3.0f * Time.deltaTime);
+			break;
+		}
+		// Increase or decrease the constant lerp timer
+		if (destination == position1){
+			// Go to position1 t = 0.0f
+			timer = Mathf.Clamp(timer - Time.deltaTime, 0.0f, 1.0f);
+		} else {
+			// Go to position2 t = 1.0f
+			timer = Mathf.Clamp(timer + Time.deltaTime, 0.0f, 1.0f);
+		}
+	}
+}
